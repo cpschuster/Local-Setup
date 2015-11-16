d2d5694f13eb67c39736878088ba1244a060b0d9
cpschuster <github@peace.neomailbox.net>
2015-11-15 19:54:27 -0800

    Adding atom text editor

--- spec/default/javascript_spec.rb
+++ spec/default/javascript_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe command("zsh -c 'nvm exec v0.12.5 npm --version'") do
   its(:stdout) { should contain('Running node v0.12.5') }
 end
+
+describe package('atom') do
+  it { should be_installed }
+end

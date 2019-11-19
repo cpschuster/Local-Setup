7f4422e96c33778064e6c303e3ddc4295a13a20a
cpschuster <github@peace.neomailbox.net>
2019-11-18 22:17:49 -0600

    Removed unused files and templates

--- provisioning/files/nvm.sh
+++
@@ -1,3 +0,0 @@
-if ([ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]) && [ -f $HOME/.nvm/nvm.sh ]; then
-    source $HOME/.nvm/nvm.sh
-fi
--- provisioning/files/virtualenv.sh
+++
@@ -1,5 +0,0 @@
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-  export WORKON_HOME=$HOME/.virtualenvs
-  export PROJECT_HOME=$HOME/Devel
-  source /usr/local/bin/virtualenvwrapper.sh
-fi

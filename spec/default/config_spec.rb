7783c52bdd3d4582fb25c82075866d547321bdc4
cpschuster <github@peace.neomailbox.net>
2019-11-18 21:26:25 -0600

    Remove Auto Login

--- spec/default/config_spec.rb
+++ spec/default/config_spec.rb
@@ -3,9 +3,5 @@ require 'spec_helper'
 describe file('/home/ubuntu/.config/terminator/config') do
   its(:content) { should contain('scrollback_infinite = True') }
   its(:content) { should contain('scroll_on_output = False') }
-  its(:content) { should contain('font = Monospace 16') }
-end
-
-describe file('/etc/lightdm/lightdm.conf.d/50_ubuntu.conf') do
-  its(:content) { should contain('autologin-user=ubuntu') }
+  its(:content) { should contain('font = Monospace 14') }
 end
